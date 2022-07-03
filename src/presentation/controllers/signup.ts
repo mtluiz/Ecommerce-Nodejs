a2a64a53a89bcb25966322d473545e9703337383
mt <matheusluizn@gmail.com>
2022-07-03 12:08:39 -0300

    feat: create email validator element injection

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,17 @@
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { Controller } from '../protocols/controller'
 import { MissingParamError } from '../errors/missing-param'
+import { InvalidParamError } from '../errors/invalid-param'
 import { badRequest } from '../helpers/http-helper'
+import { EmailValidator } from '../protocols/email-validator'
+
 export class SignUpController implements Controller {
+  private readonly emailValidator: EmailValidator
+
+  constructor(emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   handle(httpRequest: HttpRequest): HttpResponse {
     const params = ["name", "email", "password", "passwordConfirmation"]
 
@@ -10,6 +19,11 @@ export class SignUpController implements Controller {
       if (!httpRequest.body[param]) {
         return badRequest(new MissingParamError(param))
       }
+    }
+
+    const isValid = this.emailValidator.isValid(httpRequest.body.email)
+    if (!isValid) {
+      return badRequest(new InvalidParamError("email"))
     }
 
     return {
