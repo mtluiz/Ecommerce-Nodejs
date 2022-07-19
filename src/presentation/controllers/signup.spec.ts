f4870026c4c4154c657e9183ff7094554bc626f6
mt <matheusluizn@gmail.com>
2022-07-19 20:13:26 -0300

    test: check if emailvalidator is using correct email

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -94,4 +94,19 @@ describe('Signup Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new InvalidParamError('email'))
   })
+
+  test('Sould call EmailValidator with correct email', () => {
+    const { sut, emailValidatorStub } = makeSut()
+    const isValidSpy = jest.spyOn(emailValidatorStub, "isValid")
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: "anyemail@mail.com",
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    sut.handle(httpRequest)
+    expect(isValidSpy).toHaveBeenCalledWith("anyemail@mail.com")
+  })
 })
