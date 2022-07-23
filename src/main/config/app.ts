a2b265ea763a17e5db9301213ca6668f82bffff4
mt <matheusluizn@gmail.com>
2022-07-23 18:06:56 -0300

    feat: creating http request parser

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,33 @@
 import http from "http"
 
+const defaultRoute = (req, res) => {
+  res.writeHead(404, {
+    "Content-Type": "application/json"
+  })
+  res.write(JSON.stringify({
+    statusCode: 404,
+    message: "Route not found"
+  }))
+  return res.end()
+}
+
+const routes = {
+  "GET": {
+    default: defaultRoute,
+    "/hello": (req, res) => {
+      res.write("Hello node")
+      return res.end()
+    }
+  },
+  "POST": {
+    default: defaultRoute,
+  }
+}
+
 const handler = (request, response) => {
-  response.write("hello brother")
-  return response.end()
+  const { method, url } = request;
+  const chosen = routes[method][url] || routes[method].default
+  return chosen(request, response)
 }
 
 export const app = http.createServer(handler)
